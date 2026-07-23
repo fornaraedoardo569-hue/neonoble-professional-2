cdd47fcf25c97e557f0f5d19054bd94f9ffbe81f
fornaraedoardo569-hue <fornaraedoardo569@gmail.com>
2026-07-23 06:26:02 +0200

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App";
-import Dashboard from "./pages/Dashboard.tsx";
+import Dashboard from "./pages/Dashboard";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
